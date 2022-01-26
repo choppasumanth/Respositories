c2598e3f11a7999a4f4286bdc98c2508b7ff399b
choppasumanth <94858638+choppasumanth@users.noreply.github.com>
2022-01-26 06:00:19 +0530

    Create ass.js

---
+++ 17jan/ass.js
@@ -0,0 +1,10 @@
+j=[2,4]
+var result=[];
+for(let i=0;i<j.length;i++){
+    var k=(j[i]+3);
+    var l=(j[i]*4);
+    result.push([k]);
+    console.log([k]);
+    result.push([l]);
+    console.log([l]);
+}
